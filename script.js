d2ff70657daedbd9e4c2d16682fb1d9852b0d475
profeJuanDavid <educacionjuandavid@gmail.com>
2025-10-28 11:53:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+fetch("https://randomuser.me/api/?results=5&nat=mx")
+.then(respuesta => respuesta.json())
+.then(datos => {
+  
+  const contenedor = document.getElementById("contenedor")
+
+  datos.results.forEach(contacto =>{
+    const tarjeta = document.createElement("div")
+    tarjeta.className = "tarjeta"
+    tarjeta.innerHTML = `
+    <img src="${contacto.picture.large}" />
+    <p>${contacto.name.first} ${contacto.name.last}</p>
+    <p>✉️${contacto.email}</p>
+    <p>📞${contacto.phone}</p>
+    
+    `
+
+
+
+
+
+    contenedor.appendChild(tarjeta)
+
+
+  }
+
+
+  )
+  
+} )
+
+
